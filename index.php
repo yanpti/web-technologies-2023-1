dde8f3689f83d08787a8a1881a0c2d2de722f2eb
yanpti <cimdroid360@gmail.com>
2024-05-23 16:23:16 +0500

    lab 18

--- index.php
+++ index.php
@@ -1,107 +1,153 @@
 <?php
-//1
-$a = -100;
-$b = -55;
+    function first(){
+        $i = 0;
+        do {
+            if ($i == 0) {
+            echo "0 - это ноль.<br>";
+            }
+            elseif ($i % 2 == 0) {
+            echo "$i - чётное число.<br>";
+            }
+            else {
+            echo "$i - нечётное число.<br>";
+            }
+            $i++;
+        } while ($i <= 10);
+    }
 
-if ($a >= 0 && $b >= 0) {
-    echo $a - $b;
-} elseif ($a < 0 && $b < 0) {
-    echo $a * $b;
-} elseif (($a < 0 && $b >= 0) || ($a >= 0 && $b < 0)) {
-    echo $a + $b;
-}
+    function second(){
+        $regions = array(
+          "Московская область" => array("Москва", "Зеленоград", "Клин"),
+          "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+          "Рязанская область" => array("Рязань", "Касимов", "Скопин", "Рыбное", "Сасово")
+        );
+        
+        foreach ($regions as $region => $cities) {
+          echo $region . ":<br>";
+          echo implode(", ", $cities) . ".<br>";
+        }        
+    }
 
-//2
-echo '<hr/>';
-$a = 14;
-
-switch ($a <= 15 && $a > 0) {
-    case true:
-        for (; $a <= 15; $a++) {
-            echo $a;
+    function fifth() {
+        $regions = array(
+            "Московская область" => array("Москва", "Зеленоград", "Клин"),
+            "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+            "Рязанская область" => array("Рязань", "Касимов", "Скопин", "Рыбное", "Сасово")
+        );
+    
+        foreach ($regions as $region => $cities) {
+            
+            $filteredCities = array_filter($cities, function($city) {
+                return mb_substr($city, 0, 1) === "К";
+            });
+    
+            
+            if (!empty($filteredCities)) {
+                echo $region . ":<br>";
+                echo implode(", ", $filteredCities) . ".<br>";
+            }
         }
-        break;
-    case false:
-        return;
-}
-
-//3
-$a = 5;
-$b = 10;
-echo '<hr/>';
-function addition($a, $b)
-{
-    return $a + $b;
-}
-
-function subtraction($a, $b)
-{
-    return $a - $b;
-}
-
-function multiplication($a, $b)
-{
-    return $a * $b;
-}
-
-function division($a, $b)
-{
-    return $a / $b;
-}
-
-echo 'Сложение ' . addition($a, $b) . '<br/>';
-echo 'Вычитание ' . subtraction($a, $b) . '<br/>';
-echo 'Умножение ' . multiplication($a, $b) . '<br/>';
-echo 'Деление ' . division($a, $b) . '<br/>';
-
-//4
-echo '<hr/>';
-function mathOperation($arg1, $arg2, $operation)
-{
-    switch ($operation) {
-        case 'addition':
-            return addition($arg1, $arg2);
-        case 'subtraction':
-            return subtraction($arg1, $arg2);
-        case 'multiplication':
-            return multiplication($arg1, $arg2);
-        case 'division':
-            return division($arg1, $arg2);
     }
-}
-
-echo mathOperation($a, $b, 'addition');
-//6
-echo '<hr/>';
-function power($val, $pow) {
-    if ($pow == 1) {
-        return $val;
-    }
-    elseif ($pow > 0){
-        return  $val * power($val, $pow - 1);
-    }
-}
-
-echo power(2, 5);
-echo '<hr/>';
+    
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Lesson17</title>
-</head>
-<body>
-<p><?php echo date('Y'); ?></p>
-<?php require('site.php') ?>
-<?php
-$year = date('Y');
-$content = file_get_contents('site.html');
-$content = str_replace('{{ year }}', $year, $content);
-echo $content; ?>
-</body>
+
+
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <p><?php first() ?></p>
+        <p><?php second() ?></p>
+        <p><?php fifth() ?></p>
+
+        <div>
+            <?php
+            function translit($str)
+            {
+                $translit = [
+                    'а' => 'a',
+                    'б' => 'b',
+                    'в' => 'v',
+                    'г' => 'g',
+                    'д' => 'd',
+                    'е' => 'e',
+                    'ё' => 'yo',
+                    'ж' => 'zh',
+                    'з' => 'z',
+                    'и' => 'i',
+                    'й' => 'y',
+                    'к' => 'k',
+                    'л' => 'l',
+                    'м' => 'm',
+                    'н' => 'n',
+                    'о' => 'o',
+                    'п' => 'p',
+                    'р' => 'r',
+                    'с' => 's',
+                    'т' => 't',
+                    'у' => 'u',
+                    'ф' => 'f',
+                    'х' => 'h',
+                    'ц' => 'ts',
+                    'ч' => 'ch',
+                    'ш' => 'sh',
+                    'щ' => 'sch',
+                    'ъ' => '',
+                    'ы' => 'y',
+                    'ь' => '',
+                    'э' => 'e',
+                    'ю' => 'yu',
+                    'я' => 'ya'
+                ];
+
+                $result = '';
+                for ($i = 0; $i < mb_strlen($str); $i++) {
+                    $char = mb_substr($str, $i, 1);
+
+                    if (isset($translit[$char])) {
+                        $result .= $translit[$char];
+                    } else {
+                        $result .= $char;
+                    }
+                }
+                return $result;
+            }
+
+            echo translit("тесты");
+            ?>
+        </div>
+        
+        <div>
+            <?php
+                $menu = array(
+                    "1",
+                    "2",
+                    "3" => array(
+                    "3.1",
+                    "3.2",
+                    "3.3"
+                    )
+                );
+
+                echo "<ul>";
+                foreach ($menu as $key => $value) {
+                if (is_array($value)) {
+                    echo "<li>$key</li>";
+                    echo "<ul>";
+                    foreach ($value as $subkey => $subvalue) {
+                    echo "<li>$subvalue</li>";
+                    }
+                    echo "</ul>";
+                } else {
+                    echo "<li>$value</li>";
+                }
+                }
+                echo "</ul>";
+
+                
+            ?>
+        </div>
+    </body>
 </html>
