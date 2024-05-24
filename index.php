6f50e175b55e54dd46e71cb7b767b58e4a1d4e5f
yanpti <cimdroid360@gmail.com>
2024-05-24 16:49:36 +0500

    lab 19

--- index.php
+++ index.php
@@ -1,153 +1,111 @@
-<?php
-    function first(){
-        $i = 0;
-        do {
-            if ($i == 0) {
-            echo "0 - это ноль.<br>";
-            }
-            elseif ($i % 2 == 0) {
-            echo "$i - чётное число.<br>";
-            }
-            else {
-            echo "$i - нечётное число.<br>";
-            }
-            $i++;
-        } while ($i <= 10);
-    }
-
-    function second(){
-        $regions = array(
-          "Московская область" => array("Москва", "Зеленоград", "Клин"),
-          "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
-          "Рязанская область" => array("Рязань", "Касимов", "Скопин", "Рыбное", "Сасово")
-        );
-        
-        foreach ($regions as $region => $cities) {
-          echo $region . ":<br>";
-          echo implode(", ", $cities) . ".<br>";
-        }        
-    }
-
-    function fifth() {
-        $regions = array(
-            "Московская область" => array("Москва", "Зеленоград", "Клин"),
-            "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
-            "Рязанская область" => array("Рязань", "Касимов", "Скопин", "Рыбное", "Сасово")
-        );
-    
-        foreach ($regions as $region => $cities) {
-            
-            $filteredCities = array_filter($cities, function($city) {
-                return mb_substr($city, 0, 1) === "К";
-            });
-    
-            
-            if (!empty($filteredCities)) {
-                echo $region . ":<br>";
-                echo implode(", ", $filteredCities) . ".<br>";
-            }
-        }
-    }
-    
-?>
-
-
-
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <p><?php first() ?></p>
-        <p><?php second() ?></p>
-        <p><?php fifth() ?></p>
+<head>
+  <meta charset="utf-8">
+  <title>Галерея фотографий</title>
+  <style>
+    .gallery {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      align-items: center;
+      margin: 20px;
+    }
+    .gallery a {
+      margin: 10px;
+    }
+    .gallery img {
+      border: 1px solid black;
+    }
+  </style>
+</head>
+<body>
+  <?php
+    function build_gallery($dir) {
+      $handle = opendir($dir);
+      $files = array();
+      while ($file = readdir($handle)) {
+        if (preg_match("/\.(jpg|png|gif)$/i", $file)) {
+          $files[] = $file;
+        }
+      }
+      closedir($handle);
+      sort($files);
+      echo "<div class='gallery'>";
+      foreach ($files as $file) {
+        list($width, $height) = getimagesize($dir . "/" . $file);
+        $thumb_width = 200;
+        $thumb_height = $height * $thumb_width / $width;
+        echo "<a href='$dir/$file' target='_blank'>";
+        echo "<img src='$dir/$file' width='$thumb_width' height='$thumb_height'>";
+        echo "</a>";
+      }
+      echo "</div>";
+    }
 
-        <div>
-            <?php
-            function translit($str)
-            {
-                $translit = [
-                    'а' => 'a',
-                    'б' => 'b',
-                    'в' => 'v',
-                    'г' => 'g',
-                    'д' => 'd',
-                    'е' => 'e',
-                    'ё' => 'yo',
-                    'ж' => 'zh',
-                    'з' => 'z',
-                    'и' => 'i',
-                    'й' => 'y',
-                    'к' => 'k',
-                    'л' => 'l',
-                    'м' => 'm',
-                    'н' => 'n',
-                    'о' => 'o',
-                    'п' => 'p',
-                    'р' => 'r',
-                    'с' => 's',
-                    'т' => 't',
-                    'у' => 'u',
-                    'ф' => 'f',
-                    'х' => 'h',
-                    'ц' => 'ts',
-                    'ч' => 'ch',
-                    'ш' => 'sh',
-                    'щ' => 'sch',
-                    'ъ' => '',
-                    'ы' => 'y',
-                    'ь' => '',
-                    'э' => 'e',
-                    'ю' => 'yu',
-                    'я' => 'ya'
-                ];
-
-                $result = '';
-                for ($i = 0; $i < mb_strlen($str); $i++) {
-                    $char = mb_substr($str, $i, 1);
-
-                    if (isset($translit[$char])) {
-                        $result .= $translit[$char];
-                    } else {
-                        $result .= $char;
-                    }
-                }
-                return $result;
+    function upload_image($dir) {
+      if (isset($_FILES['image'])) {
+        if (preg_match("/\.(jpg|png|gif)$/i", $_FILES['image']['name'])) {
+          if ($_FILES['image']['size'] <= 5 * 1024 * 1024) {
+            $name = $_FILES['image']['name'];
+            $ext = pathinfo($name, PATHINFO_EXTENSION);
+            $new_name = uniqid() . "." . $ext;
+            move_uploaded_file($_FILES['image']['tmp_name'], $dir . "/" . $new_name);
+            list($width, $height) = getimagesize($dir . "/" . $new_name);
+            $thumb_width = 200;
+            $thumb_height = $height * $thumb_width / $width;
+            $thumb = imagecreatetruecolor($thumb_width, $thumb_height);
+            switch ($ext) {
+              case "jpg":
+                $source = imagecreatefromjpeg($dir . "/" . $new_name);
+                break;
+              case "png":
+                $source = imagecreatefrompng($dir . "/" . $new_name);
+                break;
+              case "gif":
+                $source = imagecreatefromgif($dir . "/" . $new_name);
+                break;
             }
+            imagecopyresized($thumb, $source, 0, 0, 0, 0, 
+                             $thumb_width, $thumb_height, 
+                             $width, $height);
+            switch ($ext) {
+              case "jpg":
+                imagejpeg($thumb, $dir . "/" . $new_name);
+                break;
+              case "png":
+                imagepng($thumb, $dir . "/" . $new_name);
+                break;
+              case "gif":
+                imagegif($thumb, $dir . "/" . $new_name);
+                break;
+            }
+            imagedestroy($thumb);
+            imagedestroy($source);
+            return $new_name;
+          } else {
+            echo "<p>Файл слишком большой. Максимальный размер - 5 МБ.</p>";
+          }
+        } else {
+          echo "<p>Файл не является изображением. Допустимые форматы - jpg, png, gif.</p>";
+        }
+      }
+    }
 
-            echo translit("тесты");
-            ?>
-        </div>
-        
-        <div>
-            <?php
-                $menu = array(
-                    "1",
-                    "2",
-                    "3" => array(
-                    "3.1",
-                    "3.2",
-                    "3.3"
-                    )
-                );
-
-                echo "<ul>";
-                foreach ($menu as $key => $value) {
-                if (is_array($value)) {
-                    echo "<li>$key</li>";
-                    echo "<ul>";
-                    foreach ($value as $subkey => $subvalue) {
-                    echo "<li>$subvalue</li>";
-                    }
-                    echo "</ul>";
-                } else {
-                    echo "<li>$value</li>";
-                }
-                }
-                echo "</ul>";
-
-                
-            ?>
-        </div>
-    </body>
+    $dir = "images";
+    if (!file_exists($dir)) {
+      mkdir($dir);
+    }
+    $uploaded = upload_image($dir);
+    if ($uploaded) {
+      header("Location: " . $_SERVER['PHP_SELF']);
+      exit();
+    }
+    echo "<form method='post' enctype='multipart/form-data'>";
+    echo "<input type='file' name='image'>";
+    echo "<input type='submit' value='Загрузить'>";
+    echo "</form>";
+    build_gallery($dir);
+  ?>
+</body>
 </html>
